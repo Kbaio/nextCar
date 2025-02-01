b0bf6e11be0e474084401559d2ae2b7e16e5ca6d
Estefani <2021554564@estudiantec.cr>
2025-01-31 21:32:34 -0600

    Avance pagina log in

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatCardModule],
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
