a33e0450494b19bce5d83c2564c235469f5caa70
Estefani <2021554564@estudiantec.cr>
2025-01-31 18:24:50 -0600

    Avance pagina login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
