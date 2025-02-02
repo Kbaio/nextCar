223a0086a08e0524aeb82016c4b22d9c81293c03
Anthony <anthonyvargasp.2010@gmail.com>
2025-02-01 18:13:01 -0600

    Create post - Faltan detalles

--- src/app/nextCar/pages/create-post-page/create-post-page.component.ts
+++ src/app/nextCar/pages/create-post-page/create-post-page.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
-  selector: 'app-create-post-page',
-  imports: [],
+  selector: 'input-error-state-matcher-example',
   templateUrl: './create-post-page.component.html',
-  styleUrl: './create-post-page.component.scss'
+  styleUrls: ['./create-post-page.component.scss'],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatIconModule, MatButtonModule],
 })
-export class CreatePostPageComponent {
 
+export class CreatePostPageComponent {
+  modeloFormControl = new FormControl('', [Validators.required]);
+  descripcionFormControl = new FormControl('', [Validators.required]);
+  yearFormControl = new FormControl('', [Validators.required]);
+  marcaFormControl = new FormControl('', [Validators.required]);
+  precioFormControl = new FormControl('', [Validators.required]);
+
+  matcher = new MyErrorStateMatcher();
+  imageUrl: string | ArrayBuffer | null = null;
+
+  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
+
+  triggerFileInput() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onCancel() {
+    console.log("Acción cancelada");
+    // Aquí podrías redirigir o limpiar el formulario si es necesario
+  }
+  
+  onConfirm() {
+    console.log("Acción confirmada");
+    // Aquí puedes agregar la lógica para guardar la información
+  }
+  
 }
