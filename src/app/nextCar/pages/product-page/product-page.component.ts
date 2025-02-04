42f9de04259ad3dfc018b3e271c010648f446a65
Anthony <anthonyvargasp.2010@gmail.com>
2025-02-03 22:32:57 -0600

    Product page

--- src/app/nextCar/pages/product-page/product-page.component.ts
+++ src/app/nextCar/pages/product-page/product-page.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from "../../components/product-card/product-card.component";
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
 export class ProductPageComponent {
+  product = {
+    model: 'Ejemplo Modelo',
+    description: 'Descripción del producto...',
+    year: 2024,
+    price: '₡ XX,XXX',
+    brand: 'Ejemplo Marca',
+    rating: 0,
+    seller: 'XXXX XXX'
+  };
 
+  userRating: number = 0; 
+  
+  rate(star: number): void {
+      this.userRating = star; 
+  }
+
+  showComments: boolean = false;
+  comments: string[] = ["Buen producto", "Me interesa", "Este vendedor tiene excelentes productos."];
+
+  toggleComments() {
+    this.showComments = !this.showComments;
+  }
 }
